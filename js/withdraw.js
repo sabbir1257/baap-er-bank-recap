7ac84284681219e767bae07349486a944f53301e
sabbir1257 <masabbir1257@gmail.com>
2023-02-15 00:22:16 +0600

    first commit

---
+++ js/withdraw.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-withdaw').addEventListener('click', function(){
+
+    const withdawInput = document.getElementById('deposit-input');
+    const newWithdawAmountString = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+})
