872c6166e034fc7ad1037a3e9eb9e4810fb487d7
sabbir1257 <masabbir1257@gmail.com>
2023-02-15 01:14:38 +0600

    push

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,6 +1,22 @@
-document.getElementById('btn-withdaw').addEventListener('click', function(){
+document.getElementById('btn-withdraw').addEventListener('click', function(){
 
-    const withdawInput = document.getElementById('deposit-input');
-    const newWithdawAmountString = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositAmountString);
+    const withdawInput = document.getElementById('withdraw-input');
+    const newWithdawAmountString = withdawInput.value;
+    const newWithdawAmount = parseFloat(newWithdawAmountString);
+
+    withdawInput.value = '';
+
+    const withdawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdawTotalString = withdawTotalElement.innerText;
+    const previousWithdawTotal = parseFloat(previousWithdawTotalString);
+
+    const newWithdawTotal = previousWithdawTotal + newWithdawAmount;
+    withdawTotalElement.innerText = newWithdawTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+    const newBalanceTotal = previousBalanceTotalAmount - newWithdawAmount;
+    balanceTotalElement.innerText = newBalanceTotal; 
 })
